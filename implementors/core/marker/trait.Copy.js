a0c181212f1c46822b54237d0891eb3193e727b8
Travis CI User <travis@example.org>
2018-01-12 16:18:04 +0000

    Documentation for alexcrichton/xz2-rs@d4bdfe4

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["lzma_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"lzma_sys/struct.lzma_options_lzma.html\" title=\"struct lzma_sys::lzma_options_lzma\">lzma_options_lzma</a>",];
+implementors["xz2"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"xz2/stream/enum.Action.html\" title=\"enum xz2::stream::Action\">Action</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"xz2/stream/enum.Status.html\" title=\"enum xz2::stream::Status\">Status</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"xz2/stream/enum.Check.html\" title=\"enum xz2::stream::Check\">Check</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"xz2/stream/enum.Mode.html\" title=\"enum xz2::stream::Mode\">Mode</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"xz2/stream/enum.MatchFinder.html\" title=\"enum xz2::stream::MatchFinder\">MatchFinder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["xz2"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"xz2/stream/struct.Stream.html\" title=\"struct xz2::stream::Stream\">Stream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
